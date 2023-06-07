0a2652597750e0d0da5e73880eb95a0e27307a11
sapir talker <sapirtalk@gmail.com>
2023-06-07 10:27:25 +0300

    first commit

---
+++ background.js
@@ -0,0 +1,71 @@
+chrome.runtime.onInstalled.addListener(() => {
+   console.log('Extension installed');
+ });
+ 
+ let previousCount = 0;
+
+
+function showNotification(message) {
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: 'home-button.png',
+    title: 'עדכון חדש!',
+    message: message
+  });
+
+  chrome.notifications.onClicked.addListener(function() {
+    chrome.tabs.create({ url: 'https://www.dira.moch.gov.il/ProjectsList' });
+  }
+  );
+
+
+}
+
+
+
+
+ function trackOpenLotteriesCount() {
+   fetch("https://www.dira.moch.gov.il/api/Invoker?method=Projects&param=%3FfirstApplicantIdentityNumber%3D%26secondApplicantIdentityNumber%3D%26ProjectStatus%3D4%26Entitlement%3D1%26PageNumber%3D1%26PageSize%3D50%26IsInit%3Dtrue%26", {
+    "headers": {
+      "accept": "application/json, text/plain, */*",
+      "accept-language": "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",
+      "sec-ch-ua": "\"Google Chrome\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin"
+    },
+    "referrer": "https://www.dira.moch.gov.il/ProjectsList",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+   }).then(response => {
+       if (!response.ok) {
+         throw new Error("Request failed with status code " + response.status);
+       }
+       return response.json();
+     })
+     .then(data => {
+       console.log(data); // Handle the response data
+        const openLotteriesCount = data['OpenLotteriesCount'];
+        if (openLotteriesCount > previousCount) {
+          const newLotteriesCount = openLotteriesCount - previousCount;
+          showNotification("יש " + newLotteriesCount  + "  הגרלות חדשות ! לפרטים נוספים לחץ כאן");
+        }
+        else if (openLotteriesCount <= previousCount) {
+          showNotification("לא מצאנו הגרלות חדשות");
+        }
+        previousCount = openLotteriesCount;
+     })
+     .catch(error => {
+       console.error(error); // Handle any errors
+     });
+ }
+ 
+
+ // call the function every hour
+  setInterval(trackOpenLotteriesCount, 3600000);
+  trackOpenLotteriesCount();
---
+++ popup.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+   const trackButton = document.getElementById("trackButton");
+   trackButton.addEventListener("click", () => {
+     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+       const activeTab = tabs[0];
+       chrome.scripting.executeScript({
+         target: { tabId: activeTab.id },
+         function: trackOpenLotteriesCount
+       });
+     });
+   });
+ });
+ 
